47c3b9ee291ebaf3bbaac44a8b8775606d54b048
t16cs021 <t16cs021@yamanashi.ac.jp>
2018-07-30 16:54:25 +0900

    ファイルを追加

--- Main.java
+++ Main.java
@@ -1,4 +1,16 @@
 public class Main{
+    private int data;
+    private Svm svm;
+    private Perceptron per;
+
+    Main(){
+	     this.data = 1;
+       this.svm = new Svm();
+       this.per = new Perceptron();
+     }
     public static void main(String args[]){
+
+
+
     }
 }
---
+++ Svm.java
@@ -0,0 +1,7 @@
+public class Svm{
+  Svm(){
+    
+  }
+
+
+}
